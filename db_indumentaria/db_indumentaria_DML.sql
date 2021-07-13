a7785583e239b99ffb133bb65c514b6c86309f3f
andresWeitzel <andres96energy@hotmail.com>
2021-07-13 20:18:26 +0000

    Add files via upload

--- db_indumentaria/db_indumentaria_DML.sql
+++ db_indumentaria/db_indumentaria_DML.sql
@@ -4,31 +4,56 @@
 ----------------------------
 */
 
+/*
 
 use db_indumentaria;
 
-insert into clientes(id,nombre,apellido,edad,direccion,email,telefono,tipoDocumento,numeroDocumento) values 
+insert into clientes (id,nombre,apellido,edad,direccion,email,telefono,tipoDocumento,numeroDocumento) values 
 
 	(1,'Juan','Perez',25,'Velez Dias 1123','juanPerezDump@gmail.com',1167456711,'DNI',67899076),
 	(2,'Miguelina','Candia',25,'Porta 123','MiguelinaCandia@gmail.com',1165780912,'DNI',45980467);
 
 
 
-insert into facturas(id,tipo,numero,fechaEmision,medioDePago,descripcion,idCliente) values 
+insert into articulos (id,categoria,descripcion,precio) values
+	(1,'ROPA','Sweater Blanco',2500.00),
+	(2,'ROPA','Piloto Invernal serie 2',3800.00),
+	(3,'CALZADO','Zapatillas Nasha Keep',4300.00),
+	(4,'CALZADO','Zapatos Gashi Gashi',5900.00),
+	(5,'ACCESORIOS','Sombrero Vifu',1500.00),
+	(6,'ACCESORIOS','Cartera Gucci',9500.00);
+
+
+insert into articulos_ropa_detalles (id,idArticulo,color,talle,temporada,stock,stockMinimo,stockMaximo,costo) values 
+	(1, 1,'Blanco', 'M', 'INVIERNO', 25, 5, 120, 1500.00),
+	(2, 2,'Negro', 'XL', 'INVIERNO', 10, 5, 50, 2100.00);
+
+
+insert into articulos_calzados_detalles (id,idArticulo,color,numero,temporada,stock,stockMinimo,stockMaximo,costo) values 
+	(1, 3,'Violeta/Verde', 39, 'VERANO', 30, 10, 200, 2900.00),
+	(2, 4,'Negro Mate', 43, 'INVIERNO', 3, 3, 30, 3600.00);
+
+
+insert into articulos_accesorios_detalles (id,idArticulo,color,stock,stockMinimo,stockMaximo,costo) values 
+	(1, 5,'Blanco/Morado', 20, 10, 200, 2500.00),
+	(2, 6,'Rosado', 2, 1, 10, 5600.00);
+
+
+
+insert into facturas (id,tipo,numero,fechaEmision,medioDePago,descripcion,idCliente) values 
 	
 	(1,'A',9993028302,now(),'EFECTIVO','En un solo pago',1),
 	(2,'B',7684999033,now(),'TARJETA','Debito',2);
 
 
 
-insert into articulos(id,descripcion,categoria,color,talle,numero,stock,stockMinimo,stockMaximo,costo,precio,temporada) values
-	(1,'Sweater Blanco','ROPA','Blanco','M',null,25,5,120,1900.00,2500.00,'INVIERNO'),
-	(2,'Sombrero Vifu','ACCESORIOS','Gris',null,3,15,2,120,1000.00,1500.00,'INVIERNO');
 
-
-
-insert into detalles (idArticulo, idFactura, precio, cantidad) values 
+insert into facturas_detalles (idArticulo, idFactura, precio, cantidad) values 
 	(1,1,2500,1),
 	(2,2,3000,2);
+
+*/
+
+
 
 
